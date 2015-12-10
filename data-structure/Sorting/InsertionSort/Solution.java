7ced84827e162a9ad0d1d547ab14701ecf9ee41a
vikramcse <vikramcse.10@gmail.com>
2015-12-10 19:34:18 +0530

    Silly mistake

--- data-structure/Sorting/InsertionSort/Solution.java
+++ data-structure/Sorting/InsertionSort/Solution.java
@@ -1,7 +1,7 @@
 public class Solution {
     public static void sort(int[] a) {
         int n = a.length;
-        for(int i = 1; i < n - 1; i++) {
+        for(int i = 1; i < n; i++) {
             int key = a[i];
             int j = i - 1;
             
@@ -21,7 +21,7 @@ public class Solution {
     
     public static void main(String ss[]) {
         int[] a = {6, 3, 9, 8, 2, 7, 5};
-        InsertionSort.sort(a);
-        InsertionSort.print(a);
+        Solution.sort(a);
+        Solution.print(a);
     }
 }
